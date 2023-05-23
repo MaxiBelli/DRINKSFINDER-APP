e5ecc01b04265f36c5a466ac20662427cca61762
MaxiBelli <maximiliano_belli@hotmail.com>
2023-05-23 10:40:26 -0300

    feat: Add FormDrinks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Container } from "react-bootstrap";
+import FormDrinks from "./components/FormDrinks";
 
 function App() {
   return (
@@ -6,7 +7,9 @@ function App() {
       <header className="py-5">
         <h1>Drinks Finder</h1>
       </header>
-      <Container className="mt-5"></Container>
+      <Container className="mt-5">
+        <FormDrinks />
+      </Container>
     </>
   );
 }
