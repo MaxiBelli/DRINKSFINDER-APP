ec7e147e4630a643f4dda3b25f637d2556e48278
MaxiBelli <maximiliano_belli@hotmail.com>
2023-05-23 14:34:18 -0300

    feat: Create Context and Hook for Categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { Container } from "react-bootstrap";
 import FormDrinks from "./components/FormDrinks";
+import { CategoriesProvider } from "./context/CategoriesProvider";
 
 function App() {
   return (
-    <>
+    <CategoriesProvider>
       <header className="py-5">
         <h1>Drinks Finder</h1>
       </header>
       <Container className="mt-5">
         <FormDrinks />
       </Container>
-    </>
+    </CategoriesProvider>
   );
 }
 
