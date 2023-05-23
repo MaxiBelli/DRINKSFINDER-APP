c907b112f873a9d5550f596b58dc92fe5691b748
MaxiBelli <maximiliano_belli@hotmail.com>
2023-05-23 15:11:20 -0300

    feat: Create Drinks Context and Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Container } from "react-bootstrap";
 import FormDrinks from "./components/FormDrinks";
 import { CategoriesProvider } from "./context/CategoriesProvider";
+import { DrinksProvider } from "./context/DrinksProvider";
 
 function App() {
   return (
     <CategoriesProvider>
-      <header className="py-5">
-        <h1>Drinks Finder</h1>
-      </header>
-      <Container className="mt-5">
-        <FormDrinks />
-      </Container>
+      <DrinksProvider>
+        <header className="py-5">
+          <h1>Drinks Finder</h1>
+        </header>
+        <Container className="mt-5">
+          <FormDrinks />
+        </Container>
+      </DrinksProvider>
     </CategoriesProvider>
   );
 }
---
+++ src/context/DrinksProvider.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect, createContext } from "react";
+import axios from "axios";
+
+const DrinksContext = createContext();
+
+const DrinksProvider = ({ children }) => {
+  return <DrinksContext.Provider value={{}}>{children}</DrinksContext.Provider>;
+};
+
+export { DrinksProvider };
+
+export default DrinksContext;
