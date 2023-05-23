37027471b807df5be0298e217a2267905975ed9a
MaxiBelli <maximiliano_belli@hotmail.com>
2023-05-23 10:12:46 -0300

    firts commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+function App() {
+  return (
+    <>
+      <header className="py-5">
+        <h1>Drinks Finder</h1>
+      </header>
+      <Container className="mt-5"></Container>
+    </>
+  );
+}
+
+export default App;
